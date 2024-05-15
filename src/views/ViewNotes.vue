9a8257ad4f69d014b51cd194a6925b384125fc40
UJ <sukach.eugene@gmail.com>
2024-05-15 17:48:20 +0300

    base notes styles are added by bulma css framework

---
+++ src/views/ViewNotes.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="notes">
+    <div class="card has-background-success-dark p-4 mb-5">
+      <div class="field">
+        <div class="control">
+          <textarea class="textarea" placeholder="Add a new note"></textarea>
+        </div>
+      </div>
+
+      <div class="field is-grouped is-grouped-right">
+        <div class="control">
+          <button class="button is-link has-background-success">Add New Note</button>
+        </div>
+      </div>
+    </div>
+
+    <div class="card mb-4">
+      <div class="card-content">
+        <div class="content">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut placeat quae hic
+          ducimus quod laborum natus eius dolor error, commodi voluptates, enim labore
+          ipsa maxime molestias illum incidunt fuga. Ut.
+        </div>
+      </div>
+      <footer class="card-footer">
+        <a href="#" class="card-footer-item">Edit</a>
+        <a href="#" class="card-footer-item">Delete</a>
+      </footer>
+    </div>
+  </div>
+</template>
