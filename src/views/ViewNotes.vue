ebd48a3bef78c796c0309abe22c3d955ac73a113
UJ <sukach.eugene@gmail.com>
2024-05-16 12:56:19 +0300

    basic notes logic is realized

--- src/views/ViewNotes.vue
+++ src/views/ViewNotes.vue
@@ -3,29 +3,85 @@
     <div class="card has-background-success-dark p-4 mb-5">
       <div class="field">
         <div class="control">
-          <textarea class="textarea" placeholder="Add a new note"></textarea>
+          <textarea
+            v-model="newNote"
+            class="textarea"
+            placeholder="Add a new note"
+            ref="newNoteRef"
+          ></textarea>
         </div>
       </div>
 
       <div class="field is-grouped is-grouped-right">
         <div class="control">
-          <button class="button is-link has-background-success">Add New Note</button>
+          <button
+            @click="addNote"
+            :disabled="!newNote"
+            class="button is-link has-background-success"
+          >
+            Add New Note
+          </button>
         </div>
       </div>
     </div>
 
-    <div class="card mb-4">
-      <div class="card-content">
-        <div class="content">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut placeat quae hic
-          ducimus quod laborum natus eius dolor error, commodi voluptates, enim labore
-          ipsa maxime molestias illum incidunt fuga. Ut.
-        </div>
-      </div>
-      <footer class="card-footer">
-        <a href="#" class="card-footer-item">Edit</a>
-        <a href="#" class="card-footer-item">Delete</a>
-      </footer>
-    </div>
+    <Note v-for="note in notes" :key="note.id" :note="note" @deleteClicked="deleteNote"/>
+
   </div>
 </template>
+
+<script setup>
+/**
+ * imports
+ */
+import { ref } from "vue";
+import Note from "@/components/Notes/Note.vue";
+
+/**
+ * notes
+ */
+const newNote = ref("");
+const newNoteRef = ref(null);
+
+const notes = ref([
+  {
+    id: "id1",
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut placeat quae hic ducimus quod laborum natus eius dolor error, commodi voluptates, enim labore ipsa maxime molestias illum incidunt fuga. Ut.",
+  },
+  {
+    id: "id2",
+    content: "Note #2",
+  },
+]);
+
+const addNote = () => {
+  let currentDate = new Date().getTime();
+  let id = currentDate.toString();
+
+  let note = {
+    id: id,
+    content: newNote.value,
+  };
+
+  notes.value.unshift(note);
+  newNote.value = "";
+  newNoteRef.value.focus();
+};
+
+/**
+ * delete note
+ */
+const deleteNote = (idToDelete) => {
+  notes.value = notes.value.filter(note => {return note.id !== idToDelete})
+}
+</script>
+
+<style scoped>
+.textarea {
+  resize: none;
+}
+.textarea:focus {
+  outline: 4px solid rgb(33, 150, 95);
+}
+</style>
